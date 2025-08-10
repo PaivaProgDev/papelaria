ef4ffbe3e455b10cf2eb9846d949f45fd1d94881
Guilherme <guipaiva.ferreira2021@gmail.com>
2025-08-10 20:49:48 -0300

    Bug corrigido

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import About from './components/About'
 import BestSellers from './components/BestSellers'
-import Cards from './components/Cards'
 import Header from './components/Header'
 import Hero from './components/Hero'
-import Testimonials from './components/testimonials'
+import Testimonials from './components/Testimonials'
 
 const App = () => {
   return (
