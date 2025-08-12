136bc6e633237fd629dad2e4a4c93120d898948b
Guilherme <guipaiva.ferreira2021@gmail.com>
2025-08-11 22:52:47 -0300

    Responsividade do menu mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,9 +21,9 @@ const Header = () => {
             <header className="flex items-center px-6 py-3 relative z-20 border-b bg-white border-zinc-200 justify-between">
                 <h1 className="text-2xl">Logo</h1>
                 {
-                    openMenu ? <IoMdClose onClick={handleOpenMenu} className="size-7 cursor-pointer" /> : <IoMenuSharp onClick={handleOpenMenu} className="size-7 lg:hidden block cursor-pointer" />
+                    openMenu ? <IoMdClose onClick={handleOpenMenu} className="size-7 text-red-400 cursor-pointer" /> : <IoMenuSharp onClick={handleOpenMenu} className="size-7 md:hidden block cursor-pointer" />
                 }
-                <nav className="hidden lg:block">
+                <nav className="hidden md:block">
                     <ul className="flex items-center gap-10">
                         {
                             links.map((l, index) => (
@@ -41,17 +41,20 @@ const Header = () => {
                 </nav>
             </header>
             <div onClick={() => setOpenMenu(false)} className={`${openMenu ? "visible opacity-100" : "invisible opacity-0"} fixed duration-200 w-full h-full bg-[#00000082]`}>
-                <div onClick={(e) => e.stopPropagation()} className={`${openMenu ? "lg:right-0 top-[57px]" : "lg:-right-full -top-full "} lg:top-[57px] duration-300 px-7 py-5 bg-zinc-100 ease-in-out fixed lg:w-55 lg:h-full w-full lg:bottom-0 border-l border-zinc-200`}>
+                <div onClick={(e) => {
+                    e.stopPropagation()
+                    setOpenMenu(false)
+                }} className={`${openMenu ? "lg:right-0 top-[57px]" : "lg:-right-full -top-full "} lg:top-[57px] duration-300 px-7 py-5 bg-zinc-100 ease-in-out fixed lg:w-55 lg:h-full w-full lg:bottom-0 border-l border-zinc-200`}>
                     <nav>
-                        <ul className="flex justify-between sm:justify-center gap-10  ">
+                        <ul className="grid grid-cols-2 place-self-center sm:grid-cols-3 gap-7 ">
                             {
                                 links.map((l, index) => (
-                                    <li className="text-zinc-800" key={index}>
+                                    <a href={l.href} className="text-zinc-800" key={index}>
                                         <TextBlue className='flex items-center flex-col gap-1'>
                                             {l.icon}
                                             {l.title}
                                         </TextBlue>
-                                    </li>
+                                    </a>
                                 ))
                             }
                         </ul>
